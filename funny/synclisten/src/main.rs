8fdc54a9068bd835288008fcc3a761859229a96f
zddua <zijie.duan@student.unimelb.edu.au>
2024-08-26 22:19:50 +1000

    add my funny listener

--- funny/synclisten/src/main.rs
+++ funny/synclisten/src/main.rs
@@ -5,43 +5,49 @@ use std::process::exit;
 use std::thread;
 use std::time::Duration;
 
+fn sound_map(x: f32) -> f32 {
+    let a = 0.5;
+    let b = 1.2;
+    let c = 0.2;
+    let d = 50.0;
+    let e: f32 = 2.718281828459045;
+    return b / (a + e.powf(-1.0 * (x - d))) + c;
+}
+
 fn main() {
     let host = cpal::default_host();
-    let in_device = host
-        .default_input_device()
-        .expect("no input device available");
-
-    let out_device = host
+    let device = host
         .default_input_device()
         .expect("no output device available");
 
-    let mut supported_configs_range = in_device
+    let mut supported_configs_range = device
         .supported_input_configs()
         .expect("error while querying configs");
-    let in_supported_config = supported_configs_range
+    let supported_config = supported_configs_range
         .next()
         .expect("no supported config?!")
         .with_max_sample_rate();
 
-    let mut supported_configs_range = out_device
-        .supported_input_configs()
-        .expect("error while querying configs");
-    let out_supported_config = supported_configs_range
-        .next()
-        .expect("no supported config?!")
-        .with_max_sample_rate();
+    let stream_config = StreamConfig::from(supported_config);
+    println!("dev: channel: {}", stream_config.channels);
+    println!("syp: rate   : {}", stream_config.sample_rate.0.to_string());
+    //println!("dev: buffsize: {}", stream_config.buffer_size.to_string());
 
-    let in_stream_config = StreamConfig::from(in_supported_config);
     let stream = device.build_input_stream(
         &stream_config,
         move |data: &[f32], _: &cpal::InputCallbackInfo| {
             // react to stream events and read or write stream data here.
             let mut acc = 0.;
-            let db = data.iter().for_each(|&v| acc += v);
+            let db = data.iter().for_each(|&v| acc += v.abs());
             let stn = acc as i32;
-            let stn = (stn / 5) as i32;
-            for i in 0..stn {
-                print!("*");
+            let stn = (stn / 2) as i32;
+            if stn > 1 {
+                for i in 0..stn {
+                    print!("*");
+                    if i > 115 {
+                        break;
+                    }
+                }
             }
             println!("");
         },
@@ -51,5 +57,5 @@ fn main() {
         None, // None=blocking, Some(Duration)=timeout
     );
     println!("Hello, world!");
-    thread::sleep(Duration::from_secs(2));
+    thread::sleep(Duration::from_secs(200));
 }
